43d7aee7f68ec5a286c9df0448502c7d510c2d8a
Samuel Tijani <omalitijani@gmail.com>
2020-08-14 14:52:28 +0100

    Add header, title & colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: BallPage(),
+        home: Scaffold(
+          backgroundColor: Colors.blue[700],
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
 
