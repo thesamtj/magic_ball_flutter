669597f31d4030bfc4cc66f373306c81757f282c
Samuel Tijani <omalitijani@gmail.com>
2020-08-14 15:12:36 +0100

    Make image change dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
@@ -19,10 +20,22 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void changeImage() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: FlatButton(
+        onPressed: () {
+          changeImage();
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
